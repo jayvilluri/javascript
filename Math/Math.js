60e4ba6eb2e4b20970ee7be96f167def4d2676a4
jayvilluri <kasivillurip8341@gmail.com>
2026-06-27 17:53:30 +0530

    Mathfunctions

---
+++ Math/Math.js
@@ -0,0 +1,10 @@
+function onClickSubmit(){
+    debugger;
+    alert(Number.MAX_VALUE);
+    alert(Number.MIN_VALUE);
+    alert(Number.MAX_SAFE_INTEGER);
+    alert(Number.MIN_SAFE_INTEGER);
+    alert(Number.POSITIVE_INFINITY);
+    alert(Number.NEGATIVE_INFINITY);
+    alert(Number.NaN);
+}

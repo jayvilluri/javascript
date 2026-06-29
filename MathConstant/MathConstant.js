2355d7f73f18fab957ddf1f3025fb506259af30f
jayvilluri <kasivillurip8341@gmail.com>
2026-06-29 16:51:08 +0530

    Math

---
+++ MathConstant/MathConstant.js
@@ -0,0 +1,7 @@
+function onClickSubmit(){
+    debugger;
+    alert(Math.PI)
+    alert(Math.E)
+    alert(Math.SQRT2)
+    alert(Math.LN2)
+}
---
+++ NumberFunction/NumberFunction.js
@@ -0,0 +1,19 @@
+
+function onClickNumber() {
+    debugger;
+    var a = document.getElementById("txtEnter").value;
+    number = Number(a);
+    alert(number);
+}
+function onClickParseInt() {
+    debugger;
+    var a = document.getElementById("txtEnter").value;
+    number = parseInt(a);
+    alert(number);
+}
+function onClickParseFloat() {
+    debugger;
+    var a = document.getElementById("txtEnter").value;
+    number = parseFloat(a);
+    alert(number);
+}

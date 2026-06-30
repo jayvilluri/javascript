10592de9a6d2085f7475617a4a132703eae46d04
jayvilluri <kasivillurip8341@gmail.com>
2026-06-30 16:52:30 +0530

    LogarthimicFunctions

---
+++ LogarthimicFunctions/LogarthimicFunctions.js
@@ -0,0 +1,12 @@
+function onClickLog(){
+    let a=document.getElementById("txtNum").value;
+    alert(Math.log(a));
+}
+function onClickLog10(){
+    let a=document.getElementById("txtNum").value;
+    alert(Math.log10(a));
+}
+function onClickLog2(){
+    let a=document.getElementById("txtNum").value;
+    alert(Math.log2(a));
+}

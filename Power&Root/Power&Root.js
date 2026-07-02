8cc9b3aa04f5800778ecfe6d219270161d242b44
jayvilluri <kasivillurip8341@gmail.com>
2026-07-02 14:34:05 +0530

    Power&Root

---
+++ Power&Root/Power&Root.js
@@ -0,0 +1,16 @@
+function onClickPower(){
+    debugger;
+    let a=document.getElementById("txtPower").value;
+    let b=document.getElementById("txtNum").value;
+    alert(Math.pow(a,b));
+}
+function onClickCbrt(){
+    debugger;
+    let a=document.getElementById("txtPower").value;
+    alert(Math.cbrt(a));
+}
+function onClickSqrt(){
+    debugger;
+    let a=document.getElementById("txtPower").value;
+    alert(Math.sqrt(a));
+}

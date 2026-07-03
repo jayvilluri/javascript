406677612c572d4da3cab25cc49843b7e46a568f
jayvilluri <kasivillurip8341@gmail.com>
2026-07-03 15:27:00 +0530

    TrigonometricFunctions

---
+++ TrigonometricFunctions/TrigonometricFunctions.js
@@ -0,0 +1,15 @@
+function onClickSin(){
+    debugger;
+    let a=document.getElementById("txtNum").value;
+    alert(Math.sin(a));
+}
+function onClickCos(){
+    debugger;
+    let a=document.getElementById("txtNum").value;
+    alert(Math.cos(a));
+}
+function onClickTan(){
+    debugger;
+    let a=document.getElementById("txtNum").value;
+    alert(Math.tan(a));
+}

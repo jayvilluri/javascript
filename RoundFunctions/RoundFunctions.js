2cacd3f9a3770f3c154dd92da9817f57e84e6a49
jayvilluri <kasivillurip8341@gmail.com>
2026-07-07 16:02:08 +0530

    RoundingFunctions

---
+++ RoundFunctions/RoundFunctions.js
@@ -0,0 +1,17 @@
+function onClickRound(){
+     debugger;
+    let a=document.getElementById("txtMath").value;
+    alert(Math.round(a));
+}
+function onClickCeil(){
+     let a=document.getElementById("txtMath").value;
+     alert(Math.ceil(a));
+}
+function onClickFloor(){
+      let a=document.getElementById("txtMath").value;
+     alert(Math.floor(a));
+}
+function onClickTrunc(){
+       let a=document.getElementById("txtMath").value;
+     alert(Math.trunc(a));
+}

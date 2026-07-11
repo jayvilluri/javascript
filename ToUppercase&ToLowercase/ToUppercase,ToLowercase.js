e09357c5274f7db23ab2bbd48d50caa45f88a25e
jayvilluri <kasivillurip8341@gmail.com>
2026-07-11 22:06:42 +0530

    To Upper case &To lowercase

---
+++ ToUppercase&ToLowercase/ToUppercase,ToLowercase.js
@@ -0,0 +1,12 @@
+function onClickToUpper(){
+    debugger;
+    let a=document.getElementById("txtName").value;
+    uppercase=a.toUpperCase();
+    document.getElementById("pTo").innerHTML=uppercase;
+}
+function onClickToLower(){
+    debugger;
+    let a=document.getElementById("txtName").value;
+    lowercase=a.toLowerCase();
+    document.getElementById("pTo").innerHTML=lowercase;
+}

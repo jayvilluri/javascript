304d8711138e2d3a819734aab6d963738adcd004
jayvilluri <kasivillurip8341@gmail.com>
2026-07-19 15:35:21 +0530

    fix(Concat).correct string concatenation format in onClickCheck function

--- Concat/Concat.js
+++ Concat/Concat.js
@@ -2,6 +2,6 @@ function onClickCheck(){
     debugger;
     let name=document.getElementById("txtName").value;
     let index=document.getElementById("txtIndex").value;
-    let result=name.concat("   ",index);
+    let result=name.concat(" , ",index);
     document.getElementById("pResult").innerHTML=result;
 }
